abfcb41eb8c04e34b3dbe760686f50a6df6e5974
Savan Fami <savanfami1239@gmail.com>
2024-11-08 16:42:41 +0530

    fixed cloudinary url

--- src/utils/common/cloudinaryUpload.ts
+++ src/utils/common/cloudinaryUpload.ts
@@ -14,7 +14,7 @@ export const uploadToCloudinary = async (file: any): Promise<string> => {
             CLOUDINARY_URL,
             formData
         );
-        return response.data.url;
+        return response.data.secure_url
     } catch (error) {
         console.error('Error uploading to Cloudinary:', error);
         throw new Error('Failed to upload ');
